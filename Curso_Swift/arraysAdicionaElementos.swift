70ddeb366f759cfacad637c4f63f3b37c64bec48
Maxwell Silva <maxwell.ssilvva@gmail.com>
2021-12-06 14:34:15 -0400

    Adicionando exemplo de Array em Swift

---
+++ Curso_Swift/arraysAdicionaElementos.swift
@@ -0,0 +1,14 @@
+//Arrays
+
+var Copas : [Int] = [1958, 1962, 1970, 1994, 2002]
+
+var Filmes : [String] = ["Rirmão Urso", "Big Hero 6", "How to train your Dragon"]
+
+Copas.append(2020)
+
+Copas.append(2028)
+
+Filmes.append("Miranha no Aranhaverso")
+
+print(Filmes)
+print(Copas)
