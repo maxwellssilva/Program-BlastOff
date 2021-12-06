373904132ad7547b76f158f0d566ed7531c0181d
Maxwell Silva <maxwell.ssilvva@gmail.com>
2021-12-06 14:41:52 -0400

    Adicionando exemplo de Metodo Count

---
+++ Curso_Swift/MetodoCount.swift
@@ -0,0 +1,13 @@
+//Arrays
+//Count
+var Copas : [Int] = [1958, 1962, 1970, 1994, 2002]
+
+var Filmes : [String] = ["Rirmão Urso", "Big Hero 6", "How to train your Dragon"]
+
+Copas.count
+
+Filmes.count
+
+Copas.append(Filmes.count)
+
+print(Copas)
