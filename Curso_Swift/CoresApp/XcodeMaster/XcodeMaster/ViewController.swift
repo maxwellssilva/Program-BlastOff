954a7314dfaeb7f202ce831f83dd3434061f7e3b
Maxwell Silva <83309150+maxwellssilva@users.noreply.github.com>
2022-01-03 08:22:58 -0500

    CoresAPP
    App das Cores

---
+++ Curso_Swift/CoresApp/XcodeMaster/XcodeMaster/ViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  XcodeMaster
+//
+//  Created by user209844 on 12/29/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var sliderOutlet: UISlider!
+    
+    
+    @IBOutlet weak var viewQuadrada: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        print("Hello, World!")
+        
+    }
+    
+    
+    @IBAction func btnAmarelo(_ sender: Any) {
+        
+        viewQuadrada.backgroundColor = UIColor.yellow
+        
+    }
+    
+    @IBAction func btnVermelho(_ sender: Any) {
+        
+        viewQuadrada.backgroundColor = UIColor.red
+        
+    }
+    
+    @IBAction func btnAzul(_ sender: Any) {
+        
+        viewQuadrada.backgroundColor = UIColor.blue
+        
+    }
+    
+    @IBAction func btnReset(_ sender: Any) {
+        
+        viewQuadrada.backgroundColor = UIColor.cyan
+        
+    }
+
+
+    @IBAction func slider(_ sender: Any) {
+        
+        print(sliderOutlet.value)
+        
+        viewQuadrada.alpha = CGFloat(sliderOutlet.value)
+        
+    }
+}
