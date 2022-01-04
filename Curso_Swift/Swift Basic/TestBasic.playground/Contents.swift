9355fcaa986eea85422e521a1146e79531f9ed02
Maxwell Silva <maxwell.ssilvva@gmail.com>
2022-01-04 11:23:29 -0500

    Teste 04.01.21

---
+++ Curso_Swift/Swift Basic/TestBasic.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+var chegando = "Hello, playground"
+var varNumber = 1.0
+var varNumberNew: Double
+
+varNumber = 2
+varNumberNew = 7
+
+varNumber + varNumberNew
+
+var nome1 = "Oi"
+var nome2 = "mundo"
+
+nome1 + " " + nome2
---
+++ Curso_Swift/Swift Basic/myFirstApp/myFirstApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myFirstApp
+//
+//  Created by user209844 on 1/4/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
