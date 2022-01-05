fe88839b00a1af8de5c534e592fb8eafba984b64
maxwellssilva <maxwell.ssilvva@gmail.com>
2022-01-05 15:18:34 -0500

    Atualizando a plataforma

---
+++ Swift/Swift Basic/Comando if.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+var varName = "Hello, playground"
+
+/*
+== Igual
+!= Diferente
+> Maior que
+< Menor que
+>= Maior ou igual que
+<= Menor ou igual que
+*/
+
+let meuNome = "Maxwell"
+
+if meuNome != "Maxwell" {
+    print("O nome do usuário é \(meuNome)")
+} else {
+    print("O nome do usuário não é Maxwell, mas sim \(meuNome)")
+}
--- Swift/Swift Basic/myFirstApp/myFirstApp/ViewController.swift
+++ Swift/Swift Basic/myFirstApp/myFirstApp/ViewController.swift
@@ -13,23 +13,28 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var myText: UITextView!
     
-    var myVar1 = "Estou aqui"
-    var myVar2 = "Neste lugar"
+    @IBOutlet weak var myEntry: UITextField!
+    
+    var myVar1 = "ao Curso de"
+    var myVar2 = "Swift"
     var textAux = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        myLabel.text = "Olá, Mundo! \(myVar1) \(myVar2)"
+        myLabel.text = "Bem vindos \(myVar1) \(myVar2)"
+        
+    }
+    
+    @IBAction func btnGo(_ sender: Any) {
         
         textAux = "Linha 1\n"
         textAux += "Linha 2\n"
         
         myText.text = textAux
-        
+                
     }
-
 
 }
 
